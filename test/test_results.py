32182a75b0c34ac45afaf1abf176c15503c668e7
Michael Dorner <mail@michaeldorner.de>
2023-06-15 13:36:12 +0200

    Update test_results.py

--- test/test_results.py
+++ test/test_results.py
@@ -3,7 +3,13 @@ import hashlib
 import os
 from pathlib import Path
 
-import requests
+try:
+    import requests
+    REQUESTS_INSTALLED = True
+except ModuleNotFoundError:
+    REQUESTS_INSTALLED = False
+
+ZENODO_ACCESS_TOKEN = os.environ.get('ZENODO_ACCESS_TOKEN')
 
 
 def md5(file_path):
@@ -15,24 +21,16 @@ def md5(file_path):
 
 
 class ResultsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        zenodo_access_token = os.environ.get('ZENODO_ACCESS_TOKEN')
-        if zenodo_access_token:
-            response = requests.get('https://zenodo.org/api/records/8042256', params={'access_token': zenodo_access_token}, timeout=10)
-            response.raise_for_status()
-            self.zenodo_reference = {record['key']: record['checksum'][len('md5:'):] for record in response.json()['files']}
-        else:
-            self.zenodo_reference = None
-            print('skip')
-
-        return super().setUp()
-
-    @unittest.skipIf(os.environ.get('ZENODO_ACCESS_TOKEN') is None, 'Skipped because no Zenodo access token was provided.')
+    @unittest.skipIf(not ZENODO_ACCESS_TOKEN or not REQUESTS_INSTALLED, 'Skipped because no Zenodo access token was provided.')
     def test_results(self):
-        if self.zenodo_reference:
+        zenodo_access_token = os.environ.get('ZENODO_ACCESS_TOKEN')
+        response = requests.get('https://zenodo.org/api/records/8042256', params={'access_token': zenodo_access_token}, timeout=10)
+        response.raise_for_status()
+        zenodo_reference = {record['key']: record['checksum'][len('md5:'):] for record in response.json()['files']}
+        if zenodo_reference:
             for file_path in sorted(Path('./').glob('./data/minimal_distances/*.bz2'), reverse=True):
                 file_name = file_path.name
                 with self.subTest(file_name=file_name):
                     md5_hash = md5(file_path)
-                    md5_hash_reference = self.zenodo_reference.get(file_name)
+                    md5_hash_reference = zenodo_reference.get(file_name)
                     self.assertEqual(md5_hash, md5_hash_reference, 'md5 hash is not equivalent with reference on Zenodo')
