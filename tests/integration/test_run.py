2f91df6e199b016bcca3b1852adad22319fc97d1
Michael Dorner <mail@michaeldorner.de>
2023-08-08 09:50:45 +0200

    Restructure tests

--- tests/integration/test_run.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# from simulation.run import run_simulation
-
-
-class RunTest(unittest.TestCase):
-    def test_run_simulation(self):
-        self.assertTrue(True, 'please fix')
